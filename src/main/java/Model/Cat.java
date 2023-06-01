8cf78e077b9021de8cb5feee453c8810639377f2
NataliaOrlova <natakarelskaya@yandex.ru>
2023-06-01 19:16:57 +0400

    add cat dog hamster classes

--- src/main/java/Model/Cat.java
+++ src/main/java/Model/Cat.java
@@ -1,4 +1,7 @@
 package Model;
 
-public class Cat {
+public class Cat extends Pet{
+    public Cat () {
+
+    }
 }
--- src/main/java/Model/Hamster.java
+++ src/main/java/Model/Hamster.java
@@ -1,4 +1,7 @@
 package Model;
 
-public class Hamster {
+public class Hamster extends Pet{
+    public Hamster () {
+
+    }
 }
