003b152a19d3b07c4ecc4a63933d77f9b1e3bcfc
NataliaOrlova <natakarelskaya@yandex.ru>
2023-06-02 15:16:45 +0400

    added counter  and petslist

--- src/main/java/Model/Hamster.java
+++ src/main/java/Model/Hamster.java
@@ -1,7 +1,86 @@
 package Model;
 
-public class Hamster extends Pet{
-    public Hamster () {
+import java.util.List;
 
+public class Hamster implements Pet{
+    private String name;
+    private String birthday;
+    private List<String> commands;
+
+
+    public Hamster(String name, String birthday, List<String> commands) {
+        this.name = name;
+        this.birthday = birthday;
+        this.commands = commands;
+    }
+
+
+    /**
+     * Узнать имя питомца
+     * @return
+     */
+    @Override
+    public String getName() {
+        return this.name;
+    }
+
+    /**
+     * Задать имя питомца
+     * @param name - новое имя
+     */
+    @Override
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Узнать дату рождения питомца
+     * @return
+     */
+    @Override
+    public String getBirthday() {
+        return this.birthday;
+    }
+
+    /**
+     * Установить дату рождения
+     * @param date
+     */
+    @Override
+    public void setBirthday(String date) {
+        this.birthday = date;
+    }
+
+    /**
+     * Получить список команд, которые выполняет питомец
+     * @return
+     */
+    @Override
+    public List<String> getCommandList() {
+        return this.commands;
+    }
+
+    /**
+     * Обучить новой команде
+     * @param newCommand - новая команда
+     */
+    @Override
+    public void addCommand(String newCommand) {
+        commands.add(newCommand);
+    }
+
+    /**
+     * Удалить команду
+     *
+     * @param command
+     */
+    @Override
+    public void removeCommand(String command) {
+        commands.remove(command);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("имя: %s, дата рождения: %s, команды: %s", getName(), getBirthday(), getCommandList());
     }
 }
---
+++ src/main/java/Services/PetsList.java
@@ -0,0 +1,34 @@
+package Services;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PetsList <Pet> {
+    private List<Pet> pets = new ArrayList<>();
+
+    /**
+     * добавить питомца
+     *
+     * @param pet
+     */
+    public void addAnimal(Pet pet) {
+        pets.add(pet);
+    }
+
+    /**
+     *
+     * @param pet
+     * @return
+     */
+    public boolean removePet(Pet pet){
+        return pets.remove(pet);
+    }
+
+    /**
+     * Получить список животных
+     * @return
+     */
+    public List<Pet> getPets(){
+        return pets;
+    }
+}
